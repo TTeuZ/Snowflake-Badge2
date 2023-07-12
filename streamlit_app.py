b94c718ecf69c658f1cbb2b7fe6c69ee10c3d265
TTeuZ <paulomateus.luza@hotmail.com>
2023-07-12 19:32:05 -0300

    [wip]

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,7 +38,7 @@ fruits_to_show = fruit_list.loc[fruits_selected]
 
 streamlit.dataframe(fruits_to_show)
 
-streamlit.header('Fruityvice Fruit Advice!')
+streamlit.header('View Our Fruit List - Add Your Favorites!')
 try:
     fruit_choice = streamlit.text_input('What fruit would you like information about?')
     if not fruit_choice:
@@ -51,11 +51,11 @@ except URLError as e:
 if streamlit.button('Get Fruit Load List'):
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
     my_data_rows = get_fruit_load_list()
+    my_cnx.close()
     streamlit.dataframe(my_data_rows)
-
-streamlit.stop()
 
 add_fruit = streamlit.text_input('What fruit would you like to add?', 'Jackfruit')
 if streamlit.button('Add fruit to list'):
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    my_cnx.close()
     streamlit.text(insert_row_snowflake(add_fruit))
