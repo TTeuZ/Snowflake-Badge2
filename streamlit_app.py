1b5f587f9c59d508db2db5e95289f4ce1e24850d
TTeuZ <paulomateus.luza@hotmail.com>
2023-07-12 18:45:16 -0300

    [add]

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,6 @@
 
+# Importante ressaltar que as configurações de permissáo, conta e etc utilizadas do snowflake foram feitas por meio do streamlit
+
 import streamlit
 import pandas
 import requests
@@ -36,7 +38,8 @@ streamlit.dataframe(fruityvice_normalized)
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
-my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_cur.execute("select * from fruit_load_list")
 my_data_row = my_cur.fetchone()
-streamlit.text("Hello from Snowflake:")
+streamlit.text("The fruit load list contais:")
 streamlit.text(my_data_row)
+
