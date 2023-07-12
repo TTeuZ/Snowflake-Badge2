2f72d5d88195dc129b94c25de6be33e5c3eadd45
TTeuZ <paulomateus.luza@hotmail.com>
2023-07-12 18:33:07 -0300

    [add] snowflake connector

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,8 @@ import streamlit
 import pandas
 import requests
 
+import snowflake.connector
+
 fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 fruit_list = fruit_list.set_index('Fruit')
 
