cb6e7e120a01e8b7e7d2f3369a499568697fbfc4
TTeuZ <paulomateus.luza@hotmail.com>
2023-07-12 19:17:06 -0300

    [add] try catch

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,20 +4,18 @@
 import streamlit
 import pandas
 import requests
-
 import snowflake.connector
+from urllib.error import URLError
 
 fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 fruit_list = fruit_list.set_index('Fruit')
 
 streamlit.title('My Parents New Healthy Diner')
-
 streamlit.header('Breakfast Favorites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
-
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 fruits_selected = streamlit.multiselect("Pick some fruits: ", list(fruit_list.index), ['Avocado', 'Strawberries'])
@@ -26,15 +24,18 @@ fruits_to_show = fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 streamlit.header('Fruityvice Fruit Advice!')
+try:
+    fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
+    if not fruit_choice:
+        streamlit.error("Please select a fruit to get information,")
+    else:
+        fruiyvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+        fruityvice_normalized = pandas.json_normalize(fruiyvice_response.json())
+        streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+    streamlit.error()
 
-fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
-streamlit.write('The user entered', fruit_choice)
-
-fruiyvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-
-# streamlit.text(fruiyvice_response.json())
-fruityvice_normalized = pandas.json_normalize(fruiyvice_response.json())
-streamlit.dataframe(fruityvice_normalized)
+streamlit.stop()
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
@@ -46,7 +47,4 @@ streamlit.dataframe(my_data_rows)
 add_fruit = streamlit.text_input('What fruit would you like to add?', 'Jackfruit')
 streamlit.write('Thanks for adding', add_fruit)
 
-my_data_rows.insert(0, add_fruit)
-
-streamlit.text(my_data_rows)
-streamlit.dataframe(my_data_rows)
+my_cur.execute("insert into fruit_load_list values ('from streamlit')")
