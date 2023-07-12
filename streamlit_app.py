62a82a2a42eed93fd5a1b9db0d52adeb3cf249d2
TTeuZ <paulomateus.luza@hotmail.com>
2023-07-12 14:20:46 -0300

    [add] multiselector

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import streamlit
 import pandas
 
 fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+fruit_list = fruit_list.set_index('Fruit')
 
 streamlit.title('My Parents New Healthy Diner')
 
@@ -14,4 +15,5 @@ streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
+streamlit.multiselect("Pick some fruits: ", list(fruit_list.index))
 streamlit.dataframe(fruit_list)
